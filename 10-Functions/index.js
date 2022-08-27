b131c4b0448abff71dcf96a24b317a46917b16bb
jung-ho-1231 <parkjung-1231@naver.com>
2022-08-27 22:52:26 +0900

    functions accepting callback functions

--- 10-Functions/index.js
+++ 10-Functions/index.js
@@ -29,3 +29,24 @@ const newPassport = function (person){
 
 newPassport(jonas);
 
+// =============================================
+
+const oneWord = function (str) {
+    return str.replace(/ /g, '').toLowerCase();
+}
+
+const upperFirstWord = function (str) {
+    const [first, ...others] = str.split(' ')
+    return [first.toUpperCase(), ...others].join(' ')
+}
+
+// higher-order function
+const transformer = function (str, fn) {
+    console.log(`Original string: ${str}`)
+    console.log(`Transformed string: ${fn(str)}`)
+
+    console.log(`Transformed by : ${fn.name}`)
+}
+
+transformer('Javascript is the best!', upperFirstWord)
+transformer('Javascript is the best!', oneWord)
