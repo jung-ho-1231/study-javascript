0222b309c5c691a45df64f73d46a9ae150e0cda0
jung-ho-1231 <parkjung-1231@naver.com>
2022-08-27 22:34:56 +0900

    how passing argument works

--- 10-Functions/index.js
+++ 10-Functions/index.js
@@ -14,3 +14,18 @@ const createBooks = function (flightNum, numPassengers = 1, price = 199) {
 
 createBooks('LH123')
 createBooks('LH123', undefined, 1000)
+
+// ============================================================
+
+const flight = 'LH234';
+const jonas = {
+    name : 'jung ho',
+    passport : 212321312414
+}
+
+const newPassport = function (person){
+    person.passport = Math.trunc(Math.random() * 10000);
+}
+
+newPassport(jonas);
+
